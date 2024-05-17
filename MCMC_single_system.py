b3ea510f6cb3978898ca27442dd1dd4bfee46056
Arda Erkan <ardaerkan1@hotmail.com>
2024-05-17 14:01:09 +0300

    Update - 17.05.2024

--- MCMC_single_system.py
+++ MCMC_single_system.py
@@ -9,9 +9,23 @@ import pandas as pd
 filename = "only_transit_data_28062023.csv"
 datafile = pd.read_csv(filename, skiprows=202)
 
-number_of_systems = 8
+number_of_systems = 4
 a = 0
 i = 0
+passed_systems = 0
+
+ndim = 2
+nwalkers = 1000
+burn_steps = 50
+num_steps = 250
+
+gamma_array = np.zeros((num_steps * nwalkers,))
+log_mu_tilde_array = np.zeros((num_steps * nwalkers,))
+mass_array = np.zeros((num_steps * nwalkers,))
+K_array = np.zeros((num_steps * nwalkers,))
+mean_K_list = []
+mean_masses_list = []
+which_systems = []
 
 while a != number_of_systems:
 
@@ -24,24 +38,12 @@ while a != number_of_systems:
             else:
                 P_i = datafile["pl_orbper"][k + 1] / datafile["pl_orbper"][k]
             D = 2 * (((P_i ** (2 / 3)) - 1) / ((P_i ** (2 / 3)) + 1))
-            d_list.append(D)
+            d_list.append(D) #list of orbital seperations
             row_number += 1
 
     row_number += 1 #to look at the next system at next loop
     logD_array = np.log(np.array(d_list))
 
-    ndim = 2
-    nwalkers = 1000
-    burn_steps = 50
-    num_steps = 25
-
-    gamma_array = np.zeros((num_steps*nwalkers,))
-    log_mu_tilde_array = np.zeros((num_steps * nwalkers, ))
-    mass_array = np.zeros((num_steps * nwalkers, ))
-    K_array = np.zeros((num_steps * nwalkers, ))
-    mean_K_list = []
-    mean_masses_list = []
-    size = 0
     for j in range(len(logD_array)):
 
         def log_K(log_mu_tilde,
@@ -56,8 +58,8 @@ while a != number_of_systems:
 
         def log_prior(theta):
             gamma, log_mutilde = theta
-            mean_mutilde = -4.31921754762478
-            std_mutilde = 0.28778378882844535
+            mean_mutilde = -4.3041661629482
+            std_mutilde = 0.30148352295199915
             mean_gamma = 0.5
             std_gamma = 0.3
             norm_gamma = 1. / (gamma * std_gamma * np.sqrt(2. * np.pi))
@@ -75,9 +77,9 @@ while a != number_of_systems:
                            # observed orbital separation for the system
                            osep_observed=logD_array[j]):
 
-            gamma, tilde = theta
+            gamma, log_mutilde = theta
 
-            logK = log_K(tilde, osep_observed)
+            logK = log_K(log_mutilde, osep_observed)
             K = 10. ** (logK)
             K0 = np.sqrt(12)
 
@@ -97,7 +99,7 @@ while a != number_of_systems:
 
         ##### mutilde and gamma samples #####
 
-        log_mu_tilde = scp.norm.rvs(size=nwalkers, loc=-4.31921754762478, scale=0.28778378882844535)
+        log_mu_tilde = scp.norm.rvs(size=nwalkers, loc=-4.3041661629482, scale=0.30148352295199915)
         log_mu_tilde = np.transpose(np.array(log_mu_tilde))
 
         gamma = []
@@ -111,7 +113,7 @@ while a != number_of_systems:
 
         ##### end #####
 
-        pos = np.transpose(np.vstack((log_mu_tilde, gamma)))
+        pos = np.transpose(np.vstack((gamma,log_mu_tilde)))
 
         sampler = emcee.EnsembleSampler(nwalkers, ndim, log_posterior)
         state_1 = sampler.run_mcmc(pos, burn_steps, progress=True)
@@ -121,10 +123,10 @@ while a != number_of_systems:
 
         flat_samples = flat_samples.T
 
-        mu_tilde = 10**(flat_samples[0])
-        st_mass = 0.5 * ast.M_sun.value / ast.M_earth.value #solar mass turned to earth mass units
-        masses = (1 + flat_samples[1])**(-1) * mu_tilde * st_mass
-        K = 10**log_K(flat_samples[0], logD_array[j])
+        mu_tilde = 10**(flat_samples[1])
+        st_mass = datafile["st_mass"][i] * ast.M_sun.value / ast.M_earth.value #solar mass turned to earth mass units
+        masses = (1 + flat_samples[0])**(-1) * mu_tilde * st_mass #result is in earth mass units
+        K = 10**log_K(flat_samples[1], logD_array[j])
 
         flat_samples = np.vstack((np.vstack((flat_samples,masses)),K))
         corner_samples = flat_samples.T
@@ -139,10 +141,20 @@ while a != number_of_systems:
         mean_K_list.append(mean_K)
         mean_masses_list.append(mean_masses)
 
-        labels = ["log_mutilde", "gamma", "upper_mass", "orbital_seperation"]
-        truths = [-4.31921754762478, 0.5, np.nanmean(masses), np.nanmean(K)]
+        labels = ["gamma", "log_mutilde", "upper_mass", "dynamical_seperation (K)"]
+        truths = [0.5, -4.3041661629482, np.nanmean(masses), np.nanmean(K)]
         fig = corner.corner(corner_samples, labels=labels, truths=truths)
+        print(f"System Hostname = {datafile["hostname"][i]}")
         plt.show()
 
     a += 1
     i += row_number
+
+    if len(logD_array) == 0:
+        passed_systems += 1
+        which_systems.append(0)
+    else:
+        which_systems.append(1)
+
+print(f"Passed System Count = {passed_systems}")
+print(f"Systems With More Than One Planet = {which_systems}")
